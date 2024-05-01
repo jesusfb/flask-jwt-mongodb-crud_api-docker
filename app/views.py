807ed65b448070676b34d9305c60f5d1c9e48a94
Kharapsy <kharapsy@suwalutions.com>
2024-05-01 15:02:57 +0700

    integrate restful for crud operations

--- app/views.py
+++ app/views.py
@@ -1,105 +1,105 @@
 import logging
 from datetime import timedelta
 
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask import Blueprint, request
+from flask_jwt_extended import create_access_token, jwt_required
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_restful import Resource, Api
 from app import mongo
 from app.models import Company
+
 api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
 
 logging.basicConfig(filename='access.log', level=logging.INFO)
 
-@api_bp.route('/register', methods=['POST'])
-def register():
-    data = request.json
-    name = data.get('name')
-    email = data.get('email')
-    password = generate_password_hash(data.get('password'))
+class Register(Resource):
+    def post(self):
+        data = request.json
+        name = data.get('name')
+        email = data.get('email')
+        password = generate_password_hash(data.get('password'))
 
-    user = mongo.db.users.find_one({'email': email})
-    if user:
-        return jsonify({'message': 'User already exists!'}), 400
+        user = mongo.db.users.find_one({'email': email})
+        if user:
+            return {'message': 'User already exists!'}, 400
 
-    mongo.db.users.insert_one({'name': name, 'email': email, 'password': password})
-    return jsonify({'message': 'User registered successfully!'}), 201
+        mongo.db.users.insert_one({'name': name, 'email': email, 'password': password})
+        return {'message': 'User registered successfully!'}, 201
 
-@api_bp.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    email = data.get('email')
-    password = data.get('password')
+class Login(Resource):
+    def post(self):
+        data = request.json
+        email = data.get('email')
+        password = data.get('password')
 
-    user = mongo.db.users.find_one({'email': email})
-    if not user or not check_password_hash(user['password'], password):
-        return jsonify({'message': 'Invalid credentials!'}), 401
+        user = mongo.db.users.find_one({'email': email})
+        if not user or not check_password_hash(user['password'], password):
+            return {'message': 'Invalid credentials!'}, 401
 
-    access_token = create_access_token(identity=email, expires_delta=timedelta(minutes=30))
-    logging.info(f"Access token generated for user: {email}.\nAccess token: {access_token}")
-    return jsonify(access_token=access_token), 200
+        access_token = create_access_token(identity=email, expires_delta=timedelta(minutes=30))
+        logging.info(f"Access token generated for user: {email}.\nAccess token: {access_token}")
+        return {'access_token': access_token}, 200
 
-@api_bp.route('/logout', methods=['GET'])
-@jwt_required()
-def logout():
-    # Perform logout actions if needed
-    return jsonify({'message': 'Logged out successfully!'}), 200
+class Logout(Resource):
+    @jwt_required()
+    def get(self):
+        # Perform logout actions if needed
+        return {'message': 'Logged out successfully!'}, 200
 
-@api_bp.route('/companies', methods=['POST'])
-@jwt_required()
-def create_company():
-    data = request.json
-    name = data.get('name')
-    website = data.get('website')
-    linkedin = data.get('linkedin')
-    country = data.get('country')
-    description = data.get('description')
+class CompaniesResource(Resource):
+    @jwt_required()
+    def post(self):
+        data = request.json
+        name = data.get('name')
+        website = data.get('website')
+        linkedin = data.get('linkedin')
+        country = data.get('country')
+        description = data.get('description')
 
-    existing_company_with_website = Company.find_by_website(website)
-    if existing_company_with_website:
-        return jsonify({'message': 'A company with the same website already exists!'}), 400
+        existing_company_with_website = Company.find_by_website(website)
+        if existing_company_with_website:
+            return {'message': 'A company with the same website already exists!'}, 400
 
-    existing_company_with_linkedin = Company.find_by_linkedin(linkedin)
-    if existing_company_with_linkedin:
-        return jsonify({'message': 'A company with the same LinkedIn already exists!'}), 400
+        existing_company_with_linkedin = Company.find_by_linkedin(linkedin)
+        if existing_company_with_linkedin:
+            return {'message': 'A company with the same LinkedIn already exists!'}, 400
 
-    company = Company(name, website, linkedin, country, description)
-    company.save_to_db()
+        company = Company(name, website, linkedin, country, description)
+        company.save_to_db()
 
-    return jsonify({'message': 'Company created successfully!'}), 201
+        return {'message': 'Company created successfully!'}, 201
 
-@api_bp.route('/companies', methods=['GET'])
-@jwt_required()
-def get_companies():
-    companies = Company.find_all()
-    return jsonify(companies), 200
+    @jwt_required()
+    def get(self):
+        companies = Company.find_all()
+        return companies, 200
 
-@api_bp.route('/companies/<company_id>', methods=['GET'])
-@jwt_required()
-def get_company(company_id):
-    company = Company.find_by_id(company_id)
-    if not company:
-        return jsonify({'message': 'Company not found!'}), 404
-    return jsonify(company), 200
+class CompanyResource(Resource):
+    def get(self, company_id):
+        company = Company.find_by_id(company_id)
+        if not company:
+            return {'message': 'Company not found!'}, 404
+        return company, 200
 
-@api_bp.route('/companies/<company_id>', methods=['PUT'])
-@jwt_required()
-def update_company(company_id):
-    data = request.json
-    new_data = {
-        'name': data.get('name'),
-        'website': data.get('website'),
-        'linkedin': data.get('linkedin'),
-        'country': data.get('country'),
-        'description': data.get('description')
-    }
+    def put(self, company_id):
+        data = request.json
+        new_data = {
+            'name': data.get('name'),
+            'website': data.get('website'),
+            'linkedin': data.get('linkedin'),
+            'country': data.get('country'),
+            'description': data.get('description')
+        }
+        Company.update_company(company_id, new_data)
+        return {'message': 'Company updated successfully!'}, 200
 
-    Company.update_company(company_id, new_data)
+    def delete(self, company_id):
+        Company.delete_company(company_id)
+        return {'message': 'Company deleted succesfully!'}, 200
 
-    return jsonify({'message': 'Company updated successfully!'}), 200
-
-@api_bp.route('/companies/<company_id>', methods=['DELETE'])
-@jwt_required()
-def delete_company(company_id):
-    Company.delete_company(company_id)
-
-    return jsonify({'message': 'Company deleted successfully!'}), 200
+api.add_resource(Register, '/register')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CompaniesResource, '/companies')
+api.add_resource(CompanyResource, '/companies/<company_id>')
